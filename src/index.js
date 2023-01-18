9a9093e5cf9adcce24d8fbe69b4c3a3d3dc27e7a
Erik Friis <erik.friis@hyperisland.sem>
2023-01-18 22:27:37 +0100

    fixin color bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 //import NavBar from "./components/Nav-top/NavBar";
 import "./index.css";
 import App from "./App";
-//import gobalStyles from "./styles/globalStyles.modules.css";
+import gobalStyles from "./styles/globalStyles.modules.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
