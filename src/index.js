ee52aab51791aac18724d2482137df4a882e5398
Erik Friis <erik.friis@hyperisland.sem>
2023-01-18 23:13:02 +0100

    igen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-//import NavBar from "./components/Nav-top/NavBar";
 import "./index.css";
 import App from "./App";
 import gobalStyles from "./styles/globalStyles.modules.css";
